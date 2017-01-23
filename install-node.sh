1108b9069314af3f1a78ffbc2bb389f8ebd6762f
John Häggerud <john.haggerud@lnu.se>
2017-01-23 16:13:28 +0100

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -3,21 +3,9 @@ echo "Setting environment variables..."
 echo "export NODE_ENV=development" >> /home/vagrant/.bashrc
 echo "cd /vagrant" >> /home/vagrant/.bashrc
 
-# Installing nvm
-echo "Installing nvm..."
-export HOME=/home/vagrant
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-echo "source ~/.nvm/nvm.sh" >> /home/vagrant/.bashrc
-source /home/vagrant/.nvm/nvm.sh
-
-# Installing Node.JS
-echo "Installing Node.JS..."
-nvm install stable
-chown -R vagrant:vagrant /home/vagrant/.nvm
-export HOME=/home/root
-
-# add bin folder to PATH
-echo "PATH=$PATH:/vagrant/node_modules/.bin" >> /home/vagrant/.bashrc
-PATH=$PATH:/vagrant/node_modules/.bin
+# Installing node
+echo "Installing node.js v.7.x..."
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 echo "Done installing node.js!"
